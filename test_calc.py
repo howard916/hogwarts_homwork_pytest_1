671b6c402f1cef16eb90d7755b61384fd5cf2fbc
howardwu <howardwu@crisisgord.com>
2020-12-11 19:36:22 +0800

    纠正部分错误的别名

--- test_calc.py
+++ test_calc.py
@@ -14,7 +14,7 @@ class TestCalc:
     @allure.feature("Calculator Add Feature Testcase")
     @pytest.mark.parametrize("a,b,expect", [
         (0, 0, 0), (0, 1, 1), (3, 5, 8), (-1, -2, -3), (3.52, 7.6, 11.12), (100, 300, 400)
-    ], ids=["zero", "zero+int", "int", "minus", "float", "bigint"])
+    ], ids=["zero", "zero+int", "int", "negative+negative", "float", "bigint"])
     def test_add(self, a, b, expect):
         assert expect == self.calc.add(a, b)
 
@@ -22,15 +22,15 @@ class TestCalc:
     @pytest.mark.parametrize("a,b,expect", [
         (0, 0, 0), (0, 1, -1), (5, 3, 2), (3, 5, -2), (-1, 2, -3), (3, -1, 4), (-1, -2, 1), (7.6, 3.52, 4.08),
         (3000, 1000, 2000)
-    ], ids=["zero", "zero-positive", "positive", "negative", "negative-positive", "float", "positive-negative",
-            "negative-negative", "bigint"])
+    ], ids=["zero", "zero-positive", "positive", "negative", "negative-positive", "positive-negative",
+            "negative-negative", "float", "bigint"])
     def test_sub(self, a, b, expect):
         assert expect == self.calc.sub(a, b)
 
     @allure.feature("Calculator Mul Feature Testcase")
     @pytest.mark.parametrize("a,b,expect", [
         (0, 0, 0), (0, 1, 0), (0, -1, 0), (3, 5, 15), (-1, 2, -2), (-1, -2, 2), (7.6, 3.52, 26.752), (100, 300, 30000)
-    ], ids=["zero", "zero*int", "zero*negative", "positive*positive", "negative*positive", "float", "negative*negative",
+    ], ids=["zero", "zero*int", "zero*negative", "positive*positive", "negative*positive", "negative*negative", "float",
             "bigint"])
     def test_mul(self, a, b, expect):
         assert expect == self.calc.mul(a, b)
